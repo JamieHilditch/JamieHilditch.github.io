9b625e4d316d4ad3ab4282bc958f340ba4a2a860
Jamie Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2018-08-17 15:28:02 +0100

    Update bodyscript.js

--- bodyscript.js
+++ bodyscript.js
@@ -1,1 +1,10 @@
-alert("This is an alert");
+var xpos = 0;
+var ypos = 0;
+var square = document.getElementById("square");
+var t = setInterval(move, 1000);
+function move() {
+  xpos = Math.floor(Math.random()*200);
+  ypos = Math.floor(Math.random()*200);
+  square.style.left = xpos+"px";
+  square.style.down = ypos+"px";
+}
