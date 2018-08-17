af6891c581e68274c91d114fbe9d2cea91ad788c
Jamie Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2018-08-17 15:32:24 +0100

    Update bodyscript.js

--- bodyscript.js
+++ bodyscript.js
@@ -4,7 +4,7 @@ var square = document.getElementById("square");
 var t = setInterval(move, 1000);
 function move() {
   xpos = Math.floor(Math.random()*200);
-  ypos = Math.floor(Math.random()*200);
+  ypos = Math.floor(Math.random()*-200);
   square.style.left = xpos+"px";
-  square.style.bottom = ypos+"px";
+  square.style.top = ypos+"px";
 }
